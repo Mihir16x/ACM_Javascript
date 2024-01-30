8f576f1a2b3b8d1907be2ca316928de5f7ee763f
Mihir16x <114958905+Mihir16x@users.noreply.github.com>
2024-01-30 16:48:45 -0500

    Create copy-index.js

---
+++ Mihir/copy-index.js
@@ -0,0 +1,71 @@
+// let selectedColors = ['red','blue'];
+// selectedColors[2] = 'green';
+// console.log(selectedColors[0]);
+// console.log(selectedColors.length);
+
+// //Performing a task
+// function greet(name, lastname) {
+//     console.log('Hellow ' + name + ' ' + lastname);
+// }
+
+// function square(number) {
+//     return number * number;
+// }
+
+// greet('Mark', "Smith");
+// greet('AK', 'Person');
+
+// console.log(square(2));
+
+
+//Objects
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue",
+//     isStudent: true
+//   };
+
+//   console.log(person);
+
+// //Access through dot notation
+// console.log(person.lastName);
+
+// let dude = {
+//     name: "John",
+//     age: 30,
+//     isStudent: false,
+//     address: {
+//       city: "Exampleville",
+//       country: "Exampleland"
+//     }}
+
+// console.log(dude);
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+// let text = "";
+// len = cars.length;
+
+
+// for (let i =0; i < len; i++) {
+//   text += cars[i] + ' ';
+// }
+
+// console.log(text);
+
+
+// let x = 343535
+// let y = 'mihir'
+// let z = true
+// let a = undefined
+// console.log(typeof(x))
+// console.log(typeof(y))
+// console.log(typeof(z))
+// console.log(typeof(a))
+
+
+//Equal operator information
+let thing = '4'
+console.log(4 == thing)
+console.log(4 === thing)
